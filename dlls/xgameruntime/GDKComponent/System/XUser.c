c3d67cb3ee5af4d8ac76c8e0c0973913926e3fd4
Olivia Ryan <olivia.r.dev@gmail.com>
2026-02-02 22:22:57 +0000

    xgameruntime: Implement XUserAddAsync.

--- dlls/xgameruntime/GDKComponent/System/XUser.c
+++ dlls/xgameruntime/GDKComponent/System/XUser.c
@@ -227,16 +227,122 @@ static HRESULT WINAPI x_user_XUserGetMaxUsers( IXUserImpl6 *iface, UINT32 *maxUs
     return S_OK;
 }
 
+struct XUserAddContext
+{
+    XUserAddOptions options;
+    XUserHandle user;
+};
+
+#if XODUS_INTEROP
+DEFINE_ASYNC_COMPLETED_HANDLER( async_operation_msa_token_response, IAsyncOperationCompletedHandler_IMsaTokenResponse, IAsyncOperation_IMsaTokenResponse )
+#endif
+
+static HRESULT WINAPI XUserAddProvider( XAsyncOp op, const XAsyncProviderData *data )
+{
+    struct XUserAddContext *context;
+    IXThreadingImpl *xthreading;
+    HRESULT hr;
+
+    TRACE( "op %d, data %p.\n", op, data );
+
+    if (FAILED(hr = QueryApiImpl( &CLSID_XThreadingImpl, &IID_IXThreadingImpl, (void **)&xthreading ))) return hr;
+    context = (struct XUserAddContext *)data->context;
+
+    switch (op)
+    {
+        case XAsyncOp_Begin:
+            hr = IXThreadingImpl_XAsyncSchedule( xthreading, data->async, 0 );
+            break;
+
+        case XAsyncOp_GetResult:
+            memcpy( data->buffer, &context->user, sizeof(XUserHandle) );
+            break;
+
+        case XAsyncOp_DoWork:
+#if XODUS_INTEROP
+            {
+                IAsyncOperation_IMsaTokenResponse *operation;
+                DWORD async;
+
+                if (context->options & XUserAddOptions_AddDefaultUserSilently ||
+                    context->options & XUserAddOptions_AddDefaultUserAllowingUI)
+                {
+                    if (FAILED(hr = IXodusService_MsaTokenRequest(
+                        xodus_service, msaAppId, context->options & XUserAddOptions_AddDefaultUserAllowingUI, fullTrust, &operation
+                    ))) goto fallback;
+                    if ((async = await_IAsyncOperation_IMsaTokenResponse( operation, IPC_REQUEST_TIMEOUT_MS )))
+                    {
+                        if (async == STATUS_TIMEOUT)
+                            WARN( "Timeout while waiting for MSA_TOKEN_RESPONSE response.\n" );
+                        else
+                            WARN( "Async action await failed. Status was %ld.\n", async );
+                        hr = E_ABORT;
+                    }
+                    else hr = LoadMsaUser( &context->user );
+                }
+                else hr = E_ABORT;
+                goto complete;
+
+            fallback:
+                WARN( "failed to request msa token from xodus.\n" );
+            }
+#endif
+            if (context->options & XUserAddOptions_AddDefaultUserSilently)
+                hr = LoadDefaultUser( &context->user );
+            else hr = E_ABORT;
+
+        complete:
+            IXThreadingImpl_XAsyncComplete( xthreading, data->async, hr, SUCCEEDED(hr) ? sizeof(XUserHandle) : 0 );
+            hr = S_OK;
+            break;
+
+        case XAsyncOp_Cleanup:
+            free( context );
+            break;
+
+        case XAsyncOp_Cancel:
+            break;
+    }
+
+    IXThreadingImpl_Release( xthreading );
+    return hr;
+}
+
 static HRESULT WINAPI x_user_XUserAddAsync( IXUserImpl6 *iface, XUserAddOptions options, XAsyncBlock *async )
 {
-    FIXME( "iface %p, options %d, async %p stub!\n", iface, options, async );
-    return E_NOTIMPL;
+    struct XUserAddContext *context;
+    IXThreadingImpl *xthreading;
+    HRESULT hr;
+
+    TRACE( "iface %p, options %d, async %p.\n", iface, options, async );
+
+    if (!async) return E_POINTER;
+    if (FAILED(hr = QueryApiImpl( &CLSID_XThreadingImpl, &IID_IXThreadingImpl, (void **)&xthreading ))) return hr;
+    if (!(context = calloc( 1, sizeof(*context) )))
+    {
+        IXThreadingImpl_Release( xthreading );
+        return E_OUTOFMEMORY;
+    }
+
+    context->options = options;
+    hr = IXThreadingImpl_XAsyncBegin( xthreading, async, context, NULL, "XUserAddAsync", XUserAddProvider );
+    IXThreadingImpl_Release( xthreading );
+    if (FAILED(hr)) free( context );
+    return hr;
 }
 
 static HRESULT WINAPI x_user_XUserAddResult( IXUserImpl6 *iface, XAsyncBlock *async, XUserHandle *newUser )
 {
-    FIXME( "iface %p, async %p, newUser %p stub!\n", iface, async, newUser );
-    return E_NOTIMPL;
+    IXThreadingImpl *xthreading;
+    HRESULT hr;
+
+    TRACE( "iface %p, async %p, newUser %p.\n", iface, async, newUser );
+
+    if (!async || !newUser) return E_POINTER;
+    if (FAILED(hr = QueryApiImpl( &CLSID_XThreadingImpl, &IID_IXThreadingImpl, (void **)&xthreading ))) return hr;
+    hr = IXThreadingImpl_XAsyncGetResult( xthreading, async, NULL, sizeof(*newUser), newUser, NULL );
+    IXThreadingImpl_Release( xthreading );
+    return hr;
 }
 
 static HRESULT WINAPI x_user_XUserGetLocalId( IXUserImpl6 *iface, XUserHandle user, XUserLocalId *userLocalId )
